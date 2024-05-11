b3deea94cb4fb022319f8eb417632d67475596ef
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-05-12 03:54:32 +0600

    added books q

--- index.js
+++ index.js
@@ -24,6 +24,39 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const studyscribeDb = client.db("studyscribe");
+    const booksCollection = studyscribeDb.collection("books");
+    const categories = studyscribeDb.collection("categories");
+    const featuredBooks = studyscribeDb.collection("featuredbooks");
+
+    app.get("/books", async (req, res) => {
+      const { page, size } = req.query;
+      const result = await booksCollection
+        .find()
+        .skip(parseInt(page) * parseInt(size))
+        .limit(parseInt(size))
+        .toArray();
+
+      res.send(result);
+    });
+
+    app.get("/featuredbooks", async (req, res) => {
+      const cursor = featuredBooks.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/categories", async (req, res) => {
+      const cursor = categories.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/bookscount", async (req, res) => {
+      const count = await booksCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
